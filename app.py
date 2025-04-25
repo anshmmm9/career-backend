d7f90f3b2c6cc025b9378030396be38e66209098
Anshu <anshulpotbhare123@gmail.com>
2025-04-25 12:55:45 +0530

    Port issue fixes

--- app.py
+++ app.py
@@ -70,6 +70,6 @@ def predict():
 import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 10000)), debug=True)
 
 
